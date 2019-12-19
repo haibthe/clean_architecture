86541d1a13b7ab67448e2fb01ded724af871af0d
Hai. Bui The <thehai.bui@gmail.com>
2019-12-19 18:33:25 +0700

    [5] Contracts of Data Sources #6

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -4,3 +4,8 @@ abstract class Failure extends Equatable {
 
     Failure([List properties = const<dynamic>[]]): super(properties);
 }
+
+
+class ServerFailure extends Failure {}
+
+class CacheFailure extends Failure {}
