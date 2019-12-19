58f2408574b0ddc83a2f2922ca17ea8b9297e270
Hai. Bui The <thehai.bui@gmail.com>
2019-12-19 16:34:10 +0700

    [3] Domain Layer Refactoring #4

---
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usescase.dart';
+import '../entities/number_trivia.dart';
+import '../repositories/number_trivia_repository.dart';
+
+class GetRandomNumberTrivia extends UserCase<NumberTrivia, NoParams> {
+  final NumberTriviaRepository repository;
+
+  GetRandomNumberTrivia(this.repository);
+
+  @override
+  Future<Either<Failure, NumberTrivia>> call(NoParams params) async {
+    return await repository.getRandomNumberTrivia();
+  }
+}
+
+class NoParams extends Equatable {}
