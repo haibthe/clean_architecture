71dedc397daf7b5fda1efe456bf0ac3b8b3909a2
Hai. Bui The <thehai.bui@gmail.com>
2019-12-30 14:29:52 +0700

    Add Logging #17

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ void main() async {
 void _setupLogging() {
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((rec) {
+    
     print("${rec.level.name}: ${rec.time}: ${rec.message}");
+
   });
 }
 
